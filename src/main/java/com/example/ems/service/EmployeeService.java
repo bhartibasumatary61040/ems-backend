12cec291e1c8a1f8f12e43625aa0c562bd5349b5
ems backend <bharatibasumatary61040@gmail.com>
2024-01-14 23:13:04 +0530

    first commit

---
+++ src/main/java/com/example/ems/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.example.ems.service;
+
+import com.example.ems.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long EmployeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void  deleteEmployee(Long employeeId);
+
+
+}
